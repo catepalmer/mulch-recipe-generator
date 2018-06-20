be2db2b26b3be54765e4028e89f736586798727a
anton <anton.arus@gmail.com>
2018-06-21 11:03:56 +1200

    Initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
+    <Router>
     <h1>React development has begun!</h1>
+    </Router>
   )
 }
 

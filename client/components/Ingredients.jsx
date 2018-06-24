0a122dd50a607d49f868178fb194db2d9eee15cd
talor-hammond <talorjhammond@gmail.com>
2018-06-24 18:19:43 +1200

    Started creating forms on login and ingredients pages

--- client/components/Ingredients.jsx
+++ client/components/Ingredients.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
-const Ingredients = () => {
-  return (
-    <div className='ingredients'>
-     
-    </div>
-  )
+class Ingredients extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className='ingredients'>
+        <h2>What have you got in your fridge?????????</h2>
+        <form>
+          <label for="food">Food</label>
+          <input type="text" name="name" />
+          <br/>
+          <label for="quantity">Quantity</label>
+          <input type="text" name="name" />
+          <br/>
+          <br/>
+          <input type="submit" value="Go to Ingredients Selection" />
+        </form>
+      </div>
+    )
+  }
 }
 
-export default Ingredients
+  export default Ingredients
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const Login = () => {
-  return (
-    <div className='login'>
-      
-    </div>
-  )
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className='login'>
+        <h2>Please enter your name:</h2>
+        <form action="/ingredients">
+          <input placeholder="Name" type="text" name="name" />
+          <br/>
+          <br/>
+          <input type="submit" value="Go to Ingredients Selection" />
+        </form>
+      </div>
+    )
+  }
 }
 
-export default Login
+  export default Login

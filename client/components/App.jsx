add788340d3ac2b565395a25733c4c574e8be567
catepalmer <catherinepalmer@gmail.com>
2018-06-30 19:26:09 +1200

    Created apiClient file and webpack config file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Nav from './Nav'
 import Home from './Home'
@@ -7,23 +7,29 @@ import Login from './Login'
 import Ingredients from './Ingredients'
 import Recipe from './Recipe'
 
-const App = () => {
-  return (
-    <Router>
-      <div className='app'>
-        <h1>PLAGUE'S AMAZING MULCH RECIPE GENERATOR!!!!!</h1>
-        <div className='container'>
-          <Route path='/' component={Nav}/>
-          <div className='content'>
-            <Route exact path='/' component={Home}/>
-            <Route path='/login' component={Login}/>
-            <Route path='/ingredients' component={Ingredients}/>
-            <Route path='/recipe' component={Recipe}/>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='app'>
+          <h1>PLAGUE'S AMAZING MULCH RECIPE GENERATOR!!!!!</h1>
+          <div className='container'>
+            <Route path='/' component={Nav} />
+            <div className='content'>
+              <Route exact path='/' component={Home} />
+              <Route path='/login' component={Login} />
+              <Route path='/ingredients' component={Ingredients} />
+              <Route path='/recipe' component={Recipe} />
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
-  )
+      </Router>
+    )
+  }
 }
 
 export default App
